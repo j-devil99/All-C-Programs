d5f291a60e03cda1abecf56b2a16797f683ee446
rdutta99 <32390451+rdutta99@users.noreply.github.com>
2019-10-12 23:00:24 +0530

    Update 5.cpp
    For even and odd numbers

--- Numbers/5.cpp
+++ Numbers/5.cpp
@@ -1,15 +1,18 @@
-//write a program to find sum of all numbers which is divisible by 2 between two numbers
+//write a program to find sum of all odd and even numbers between two numbers
 #include<stdio.h>
 int main(){
-	int start,end,i,sum=0;
+	int start,end,i,sum=0,sum1=0;
 	printf("Enter first number :");
 	scanf("%d",&start);
 	printf("Enter last number :");
 	scanf("%d",&end);
 	for(i=start;i<=end;i++){
 		if(i%2==0)
-		sum=sum+i;
+		sum+=i;
+		else
+		sum1+=i;
 	}
-	printf("Sum of even numbers between %d and %d : %d",start,end,sum);
+	printf("Sum of even numbers between %d and %d : %d\n",start,end,sum);
+	printf("Sum of odd numbers between %d and %d : %d",start,end,sum1);
 	return 0;
 }
